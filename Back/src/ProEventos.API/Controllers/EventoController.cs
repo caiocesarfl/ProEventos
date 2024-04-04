9b44a972f136396f6fc0f83d4a47bd7d3a808d1c
Caio Lara <caio.lara@cmuenergia.com.br>
2024-04-04 12:16:33 -0300

    Install Bootsrap

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -11,10 +11,10 @@ namespace ProEventos.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class EventoController : ControllerBase
+    public class eventosController : ControllerBase
     {
         public readonly DataContext _context;
-        public EventoController(DataContext context)
+        public eventosController(DataContext context)
         {
             _context = context;
         }
