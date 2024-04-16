acec32e40bdd4ec27c688cfc473dd33ebfa2cf71
Caio Lara <caio.lara@cmuenergia.com.br>
2024-04-16 18:06:19 -0300

    Updade perfil form

--- Front/ProEventos-App/src/app/componentes/perfil/perfil.component.ts
+++ Front/ProEventos-App/src/app/componentes/perfil/perfil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-perfil',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil.component.scss']
 })
 export class PerfilComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
+  constructor(public fb: FormBuilder) { }
+
+  get f(): any {return this.form.controls;}
+
 
   ngOnInit() {
+    this.validation();
+  }
+
+  public validation() :void  {
+    this.form = this.fb.group({
+      primeiroNome : ['', Validators.required],
+      ultimoNome : ['', Validators.required],
+      email : ['', Validators.required],
+      telefone : ['', Validators.required],
+      funcao : ['', Validators.required],
+      descricao : ['', Validators.required],
+      senha : ['', Validators.required],
+      confirmarSenha: ['', Validators.required]
+    })
+
   }
 
 }
